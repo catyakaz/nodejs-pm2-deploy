04529ebba98f75e08d0e751e970bd2b63c2f94cc
catyakaz <catyakaz1@gmail.com>
2024-06-03 16:56:14 +0000

    deploy 2 fix

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -3,14 +3,20 @@ const dotenv = require('dotenv');
 dotenv.config({ path: '.env.deploy' });
 
 const {
-  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF, DEPLOY_REPO,
+  DEPLOY_USER,
+  DEPLOY_HOST,
+  DEPLOY_PATH,
+  DEPLOY_REF,
+  DEPLOY_REPO,
 } = process.env;
 
 module.exports = {
   apps: [{
-    name: "frontend",
+    name: 'frontend',
   }],
-
+  env: {
+    NODE_ENV: 'production',
+  },
   deploy: {
     production: {
       user: DEPLOY_USER,
@@ -18,8 +24,8 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
-      'pre-deploy-local': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`
-      'post-deploy': 'export PATH=$PATH:~/.nvm/versions/node/v18.20.3/bin/ && npm i && npm run build && pm2 restart all',
+      'post-deploy': 'export PATH=$PATH:~/.nvm/versions/node/v18.20.3/bin/ && npm i && npm run build',
     },
   },
-}
+};
+
